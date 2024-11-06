9a0e75d7b7e8f4d3dbb7f753c3e46393b35f0eb4
Abdellioua Yasser <yasserabdellioua2@gmail.com>
2024-11-06 23:30:41 +0100

    feat(provision):creation of vm storage account and upload file to remote backend

--- Terraform/storage.tf
+++ Terraform/storage.tf
@@ -1,14 +1,14 @@
 
 # Resource group for the storage account
 resource "azurerm_resource_group" "rg-tfstate" {
-  name     = "rg-tfstate-hylastik-project"
+  name     = "rgtfstatehylastikproject"
   location = "East US"  # Change location as needed
 }
 
 # This file to show how to create storage account with terraform  where we can store our state-file
 # Storage account for Terraform state
 resource "azurerm_storage_account" "storage-state-file" {
-  name                     = "tfstate-hylastik-project"
+  name                     = "tfstatehylastikproject"
   resource_group_name      = azurerm_resource_group.rg-tfstate.name
   location                 = azurerm_resource_group.rg-tfstate.location
   account_tier             = "Standard"
@@ -17,7 +17,7 @@ resource "azurerm_storage_account" "storage-state-file" {
 
 # Container for storing the state file
 resource "azurerm_storage_container" "container" {
-  name                  = "tfstate-container"
+  name                  = "tfstatecontainer"
   storage_account_name  = azurerm_storage_account.storage-state-file.name
   container_access_type = "private"
 }
