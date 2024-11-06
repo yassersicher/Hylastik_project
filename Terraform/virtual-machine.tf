3295704740083e878ead04e922c0a78c79589046
Abdellioua Yasser <yasserabdellioua2@gmail.com>
2024-11-07 00:12:52 +0100

    feat(provision):allow-port-8088 and allow-port-8001

--- Terraform/virtual-machine.tf
+++ Terraform/virtual-machine.tf
@@ -61,9 +61,10 @@ resource "azurerm_network_interface" "interface" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    network_security_group_id = azurerm_network_security_group.security_group.id 
+    
 
   }
+  network_security_group_id = azurerm_network_security_group.security_group.id 
 }
 resource "azurerm_virtual_machine" "virtual_machine" {
   name                  = "${var.prefix}-vm"
